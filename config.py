5e0b75e2b5e3c8536e895c621842b3bd223118d5
Eric Schanet <eric.schanet@cern.ch>
2019-07-19 10:55:16 +0200

    Some minor changes in the default config

--- config.py
+++ config.py
@@ -14,10 +14,11 @@ Double_t getMlb1(Double_t jet1MV2c10, Double_t lj1_eta, Double_t lj2_eta, Double
 # Some selections used later in the config
 SRSelection  = "nJet30>=2 && nJet30<=3 && nBJet30_MV2c10==2 && mbb>100. && mbb<140. && met>240."
 OneLepSelection = "&&  nLep_base==1&&nLep_signal==1 && ( (AnalysisType==1 && lep1Pt>7.) || (AnalysisType==2 && lep1Pt>6.))"
-mct_bins = ["&& mct2>180. && mct2<=220.", "&& mct2>220. && mct2<=280.", "&& mct2>=280."]
+mct_bins = ["&& mct2>180. && mct2<=230.", "&& mct2>230. && mct2<=280.", "&& mct2>=280."]
 
 #Directory to my trees
-_treefilename = "/project/etp4/eschanet/ntuples/common/full_run_2/v1-20/allTrees_v1_20_1_combined_bkg_skimmed.root"
+_treefilename = "/project/etp5/SUSYEWKWH/1Lbb/trees/v2-0/nomTrees_v2_0_bkg_1Lbb_skim.root"
+_sigfilename = "/project/etp5/SUSYEWKWH/1Lbb/trees/v2-0/allTrees_v2_0_signal_1Lbb_skim.root"
 
 #Selections I want to have in my yields table. Order will be the same in the table!
 selections = collections.OrderedDict()
@@ -27,10 +28,9 @@ selections["SRLM_bin3"] = SRSelection + OneLepSelection + "&& mt>=100. && mt<160
 selections["SRMM_bin1"] = SRSelection + OneLepSelection + "&& mt>=160. && mt<240. && met>240." + mct_bins[0]
 selections["SRMM_bin2"] = SRSelection + OneLepSelection + "&& mt>=160. && mt<240. && met>240." + mct_bins[1]
 selections["SRMM_bin3"] = SRSelection + OneLepSelection + "&& mt>=160. && mt<240. && met>240." + mct_bins[2]
-selections["SRHM_bin1"] = SRSelection + OneLepSelection + "&& mt>=240. && met>240. && getMlb1(jet1MV2c10,lep1Eta-jet1Eta,lep1Eta-jet2Eta,lep1Phi-jet1Phi,lep1Phi-jet2Phi,jet1Pt,jet2Pt,lep1Pt)>120.0" + mct_bins[0]
-selections["SRHM_bin2"] = SRSelection + OneLepSelection + "&& mt>=240. && met>240. && getMlb1(jet1MV2c10,lep1Eta-jet1Eta,lep1Eta-jet2Eta,lep1Phi-jet1Phi,lep1Phi-jet2Phi,jet1Pt,jet2Pt,lep1Pt)>120.0" + mct_bins[1]
-selections["SRHM_bin3"] = SRSelection + OneLepSelection + "&& mt>=240. && met>240. && getMlb1(jet1MV2c10,lep1Eta-jet1Eta,lep1Eta-jet2Eta,lep1Phi-jet1Phi,lep1Phi-jet2Phi,jet1Pt,jet2Pt,lep1Pt)>120.0" + mct_bins[2]
-
+selections["SRHM_bin1"] = SRSelection + OneLepSelection + "&& mt>=240. && met>240. && mlb1>120.0" + mct_bins[0]
+selections["SRHM_bin2"] = SRSelection + OneLepSelection + "&& mt>=240. && met>240. && mlb1>120.0" + mct_bins[1]
+selections["SRHM_bin3"] = SRSelection + OneLepSelection + "&& mt>=240. && met>240. && mlb1>120.0" + mct_bins[2]
 
 ################################################################################################
 #
@@ -54,10 +54,15 @@ config = {
         ("tth", "background", [(_treefilename, "tth_NoSys")]),
         ("zjets", "background", [(_treefilename, "zjets_NoSys")]),
         ("vh", "background", [(_treefilename, "vh_NoSys")]),
+        ("Signal (800,0)", "signal", [(_sigfilename, "C1N2_Wh_hbb_800p0_0p0_NoSys")]),
+        ("Signal (800,150)", "signal", [(_sigfilename, "C1N2_Wh_hbb_800p0_150p0_NoSys")]),
+        ("Signal (700,250)", "signal", [(_sigfilename, "C1N2_Wh_hbb_700p0_250p0_NoSys")]),
+        ("Signal (500,200)", "signal", [(_sigfilename, "C1N2_Wh_hbb_500p0_200p0_NoSys")]),
+        ("Signal (350,150)", "signal", [(_sigfilename, "C1N2_Wh_hbb_350p0_150p0_NoSys")]),
     ],
     "selections" : selections,
-    "lumifactor" : 140500,
+    "lumifactor" : 139000,
     "weights" : "genWeight*eventWeight*pileupWeight*leptonWeight*bTagWeight*jvtWeight",
-    "output_path" : "/project/etp5/eschanet/studies/1Lbb_Wh_Run2_reopt_full_new_vars/yieldstables/test_module/",
+    "output_path" : "/project/etp5/eschanet/studies/1Lbb_Wh_Run2_reopt_full_new_vars/yieldstables/optimised_SRs_mctbins_sym/",
     "output_name" : "yields",
 }
